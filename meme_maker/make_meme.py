7536738502de7f5b60bb0fa17f40872d6fee2ee1
Erkenbend <adri.w51@hotmail.fr>
2020-11-22 00:02:09 +0100

    make_meme is now a function

--- meme_maker/make_meme.py
+++ meme_maker/make_meme.py
@@ -1,50 +1,48 @@
 import MemePy as mp
 from PIL import Image
 
-dob = '12/34/5678'
-dod = '12/34/6789'
+TOP_BOX_SIZE = 200
+BOTTOM_BOX_SIZE = 1000
+TEXT_COLOR = [255, 255, 255]
 
-im = Image.open('./resources/ImageLibrary/dead_person.jpg')
-width, height = im.size
-#print(width, height)
+def make_meme(birth_date, death_date, image_path):
+    im = Image.open(image_path)
+    width, height = im.size
+    #print(width, height)
 
-with open('./resources/meme-def-template.json', 'r') as f:
-    lines_with_placeholders = f.readlines()
+    with open('./resources/meme-def-template.json', 'r') as f:
+        lines_with_placeholders = f.readlines()
 
-nb_pos_encounters = 0
-nb_dim_encounters = 0
-top_box_size = 200
-bottom_box_size = 900
-text_color = [255, 255, 255]
+    nb_pos_encounters = 0
+    nb_dim_encounters = 0
 
-lines_to_export = []
-for line in lines_with_placeholders:
-    if "\"pos\"" in line:
-        if nb_pos_encounters < 1:
-            line = line.replace("TOP_X", str((width - top_box_size) // 2), 1)
-            line = line.replace("TOP_Y", str(height // 20), 1)
-        else:
-            line = line.replace("BOTTOM_X", str((width - bottom_box_size) // 2), 1)
-            line = line.replace("BOTTOM_Y", str(7.5 * height // 10), 1)
-        nb_pos_encounters += 1
-    elif "\"dimensions\"" in line:
-        if nb_dim_encounters < 1:
-            line = line.replace("TOP_BOX_SIZE", str(top_box_size))
-        else:
-            line = line.replace("BOTTOM_BOX_SIZE", str(bottom_box_size))
-        nb_dim_encounters += 1
-    elif "\"text_color\"" in line:
-        line = line.replace("TEXT_COLOR", str(text_color))
-    lines_to_export.append(line)
+    lines_to_export = []
+    for line in lines_with_placeholders:
+        if "\"pos\"" in line:
+            if nb_pos_encounters < 1:
+                line = line.replace("TOP_X", str((width - TOP_BOX_SIZE) // 2), 1)
+                line = line.replace("TOP_Y", str(height // 20), 1)
+            else:
+                line = line.replace("BOTTOM_X", str((width - BOTTOM_BOX_SIZE) // 2), 1)
+                line = line.replace("BOTTOM_Y", str(7.5 * height // 10), 1)
+            nb_pos_encounters += 1
+        elif "\"dimensions\"" in line:
+            if nb_dim_encounters < 1:
+                line = line.replace("TOP_BOX_SIZE", str(TOP_BOX_SIZE))
+            else:
+                line = line.replace("BOTTOM_BOX_SIZE", str(BOTTOM_BOX_SIZE))
+            nb_dim_encounters += 1
+        elif "\"text_color\"" in line:
+            line = line.replace("TEXT_COLOR", str(TEXT_COLOR))
+        lines_to_export.append(line)
 
-#print(lines_to_export)
+    # print(lines_to_export)
 
-with open('./resources/MemeLibrary/meme-def.json', 'w') as f:
-    f.writelines(lines_to_export)
+    with open('./resources/MemeLibrary/meme-def.json', 'w') as f:
+        f.writelines(lines_to_export)
 
+    mp.add_external_resource_dir('./resources')
+    mp.save_meme_to_disk('DeadPerson', '.', ['R.I.P.', birth_date + '\n' + death_date + ' '])
 
-mp.add_external_resource_dir('./resources')
-mp.save_meme_to_disk('DeadPerson', '.', ['R.I.P.', dob+'\n'+dod])
-
-
-
+if __name__ == '__main__':
+    make_meme('2000-02-28', '2000-05-05', './resources/ImageLibrary/dead_person.jpg')
