6f7d5232f7808703a91ff91a80bc5b8014a99c7a
Erkenbend <adri.w51@hotmail.fr>
2020-11-22 02:41:02 +0100

    make wiki search as function

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 9gag-bot
 
 * need python 3.6+
-* need 9gag credentials as `username:password` under `uploader/resources/credentials.txt`
-* need twitter api key under `uploader/resources/twitter_credentials.yml`
+* need 9gag credentials as `username:password` under `resources/credentials.txt`
+* need twitter api key under `resources/twitter_credentials.yml`
--- wiki_search.py
+++ wiki_search.py
@@ -17,65 +17,69 @@ class JsonWrapper:
     def __init__(self, dead_persons):
         self.dead_persons = dead_persons
 
-# build wiki url
-today = datetime.datetime.now()
-month = today.strftime("%B")
-day = int(today.strftime("%d"))
-url = f"https://en.wikipedia.org/wiki/{month}_{day}#Deaths"
-print(url)
-
-# query page
-pq = PyQuery(url)
-tag = pq('#Deaths').parent().next("ul")
-
-childs = tag.children()
-dead_persons = []
-
-for child in childs:
-    links = child.findall("a")
-    #print(links)
-
-    if len(links) >= 2: 
-        year = links[0].text_content()
-        try:
-            if int(year) >= 1990:
-
-                dday = f"{year}-{today.strftime('%m')}-{day}"
-                name = links[1].text_content()
-                p_url = f"https://en.wikipedia.org{links[1].attrib.get('href')}"
-                print(p_url)
-                p_site = PyQuery(p_url)
-                text_length = len(p_site.text()) # => costs time
-                img_site = f'https://en.wikipedia.org{p_site.find("table.infobox.vcard").find("a")[0].attrib.get("href")}'
-
-                img_url = PyQuery(img_site).find("div#file").find("a")[0].attrib.get("href")
-                img_url = f'https:{img_url}'
-                print(img_url)
-                
-                bday = p_site.find("table.infobox.vcard").find("span.bday")[0].text
-                dead_persons.append(DeadPerson(dday, name, p_url, text_length, img_url, bday))
-        except ValueError:
-            continue
-        except IndexError:
-            continue
-
-print("parsed all dead persons")
-
 def sort_by_text_lenght(e):
     return e.text_length
 
-dead_persons.sort(reverse=True,key=sort_by_text_lenght)
+def wiki_search(today):
+    month = today.strftime("%B")
+    day = int(today.strftime("%d"))
+    url = f"https://en.wikipedia.org/wiki/{month}_{day}#Deaths"
+    print(url)
 
-print(dead_persons[0].__dict__)
+    # query page
+    pq = PyQuery(url)
+    tag = pq('#Deaths').parent().next("ul")
 
-my_d_person = dead_persons[0]
-r = requests.get(my_d_person.img_url, allow_redirects=True)
-img_path = "./resources/ImageLibrary/dead_person.jpg"
+    childs = tag.children()
+    dead_persons = []
 
-open(img_path, 'wb').write(r.content)
-print("downloaded file. now start making meme")
+    for child in childs:
+        links = child.findall("a")
+        # print(links)
 
-#make_meme(str(my_d_person.bday), str(my_d_person.dday), img_path)
-#make_meme('2000-02-28', '2000-05-05', './resources/ImageLibrary/dead_person.jpg')
+        if len(links) >= 2:
+            year = links[0].text_content()
+            try:
+                if int(year) >= 1990:
+                    dday = f"{year}-{today.strftime('%m')}-{day}"
+                    name = links[1].text_content()
+                    p_url = f"https://en.wikipedia.org{links[1].attrib.get('href')}"
+                    print(p_url)
+                    p_site = PyQuery(p_url)
+                    text_length = len(p_site.text())  # => costs time
+                    img_site = f'https://en.wikipedia.org{p_site.find("table.infobox.vcard").find("a")[0].attrib.get("href")}'
 
-print("Finished")
+                    img_url = PyQuery(img_site).find("div#file").find("a")[0].attrib.get("href")
+                    img_url = f'https:{img_url}'
+                    print(img_url)
+
+                    bday = p_site.find("table.infobox.vcard").find("span.bday")[0].text
+                    dead_persons.append(DeadPerson(dday, name, p_url, text_length, img_url, bday))
+            except ValueError:
+                continue
+            except IndexError:
+                continue
+
+    print("parsed all dead persons")
+
+    dead_persons.sort(reverse=True, key=sort_by_text_lenght)
+
+    print(dead_persons[0].__dict__)
+
+    my_d_person = dead_persons[0]
+    r = requests.get(my_d_person.img_url, allow_redirects=True)
+    img_path = "./resources/ImageLibrary/dead_person.jpg"
+
+    open(img_path, 'wb').write(r.content)
+    #print("downloaded file. now start making meme")
+
+    # make_meme(str(my_d_person.bday), str(my_d_person.dday), img_path)
+    # make_meme('2000-02-28', '2000-05-05', './resources/ImageLibrary/dead_person.jpg')
+
+    print("Finished")
+
+    return my_d_person
+
+if __name__ == '__main__':
+    print(wiki_search(datetime.datetime.now()))
+
